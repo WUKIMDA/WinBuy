8a66845e03b6a337c69a42c7b4df88dd5633da1f
kimda <616792989@qq.com>
2017-06-15 11:12:13 +0800

    虚拟按键隐藏的加入和浸入状态栏

--- app/src/main/java/buy/win/com/winbuy/view/activity/MainActivity.java
+++ app/src/main/java/buy/win/com/winbuy/view/activity/MainActivity.java
@@ -1,7 +1,9 @@
 package buy.win.com.winbuy.view.activity;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import buy.win.com.winbuy.R;
 
@@ -13,4 +15,35 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         System.out.print("git冲突测试");
     }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        hideBottomUIMenu();
+    }
+
+    /**
+     * 隐藏虚拟按键，并且全屏
+     */
+    protected void hideBottomUIMenu() {
+        //隐藏虚拟按键，并且全屏
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // 版本判断
+            View v = this.getWindow().getDecorView();
+            v.setSystemUiVisibility(View.GONE);//如果是低版本  全部隐藏
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            //for new api versions.
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY ;
+//            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+            //View.SYSTEM_UI_FLAG_HIDE_NAVIGATION  导航栏
+            //View.SYSTEM_UI_FLAG_FULLSCREEN    底部虚拟按键
+            //View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY 触摸就显示
+        }
+    }
+
 }
