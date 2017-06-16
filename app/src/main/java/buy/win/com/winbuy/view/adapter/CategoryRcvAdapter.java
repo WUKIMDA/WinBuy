ad1dddd20d10694476412c8cc20f0f879be9c86f
SeasonAndSun <571561862@qq.com>
2017-06-16 14:20:57 +0800

    罗林海测试!

---
+++ app/src/main/java/buy/win/com/winbuy/view/adapter/CategoryRcvAdapter.java
@@ -0,0 +1,150 @@
+package buy.win.com.winbuy.view.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import buy.win.com.winbuy.R;
+import buy.win.com.winbuy.model.net.CategoryAllBean;
+import buy.win.com.winbuy.utils.Constans;
+
+/**
+ * Created by Administrator on 2017/6/15 0015.
+ */
+
+public class CategoryRcvAdapter extends RecyclerView.Adapter {
+
+    private static final int TYPE_HEADVIEW = 0;
+    private static final int TYPE_COMMON = 1;
+    private Context mContenx;
+    private List<CategoryAllBean.CategoryBean> mDatas = new ArrayList<>();
+    private int mSelectedId;
+    private List<CategoryAllBean.CategoryBean> mRcvDatas = new ArrayList<>();
+
+    public CategoryRcvAdapter(Context contenx) {
+        mContenx = contenx;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return TYPE_HEADVIEW;
+        }
+        return TYPE_COMMON;
+    }
+
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder viewholder = null;
+        switch (viewType) {
+            case TYPE_HEADVIEW:
+                View headView = LayoutInflater.from(mContenx).inflate(R.layout.category_rcv_headview, parent, false);
+                viewholder = new HeadViewHolder(headView);
+                break;
+            case TYPE_COMMON:
+                View commonView = LayoutInflater.from(mContenx).inflate(R.layout.category_rcv_itemview, parent, false);
+                viewholder = new CommonViewHolder(commonView);
+                break;
+        }
+        return viewholder;
+    }
+
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (position == 0) {
+            HeadViewHolder headView = (HeadViewHolder) holder;
+            headView.setData(mDatas, mSelectedId);
+        } else {
+            CommonViewHolder headView = (CommonViewHolder) holder;
+            headView.setData(mDatas, mRcvDatas.get(position));
+//            Toast.makeText(mContenx,mRcvDatas.toString(),Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mRcvDatas != null) {
+            return mRcvDatas.size();
+        }
+        return 0;
+    }
+
+    public void setDatas(List<CategoryAllBean.CategoryBean> datas, int selectedId, List<CategoryAllBean.CategoryBean> rcvDatas) {
+        mDatas = datas;
+        mSelectedId = selectedId;
+        mRcvDatas.clear();
+        rcvDatas.add(0,null);
+        mRcvDatas.addAll(rcvDatas);
+        notifyDataSetChanged();
+    }
+
+
+    class HeadViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.headview)
+        ImageView mHeadview;
+
+        HeadViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+
+        public void setData(List<CategoryAllBean.CategoryBean> data, int id) {
+            for (int i = 0; i < data.size(); i++) {
+                if (data.get(i).getId() == id) {
+                    Glide.with(mContenx).load(Constans.URL_HOST + data.get(i).getPic()).into(mHeadview);
+                }
+            }
+        }
+    }
+
+    class CommonViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.category_rcv_item_tv)
+        TextView mCategoryRcvItemTv;
+        @Bind(R.id.category_rcv_item_gv)
+        GridView mCategoryRcvItemGv;
+
+        CommonViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+
+
+        public void setData(List<CategoryAllBean.CategoryBean> datas, CategoryAllBean.CategoryBean categoryBean) {
+            mCategoryRcvItemTv.setText(categoryBean.getName());
+
+            List<CategoryAllBean.CategoryBean> mGridDatas = new ArrayList<>();
+            for (int i = 0; i < datas.size(); i++) {
+                if (datas.get(i).getParentId() == categoryBean.getId()) {
+                    mGridDatas.add(datas.get(i));
+                }
+            }
+
+            GridViewAdapter gridViewAdapter = new GridViewAdapter(mContenx, mGridDatas);
+            mCategoryRcvItemGv.setAdapter(gridViewAdapter);
+            mCategoryRcvItemGv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    //// TODO: 2017/6/15 0015
+
+                }
+            });
+        }
+    }
+}
