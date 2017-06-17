ff2c33339afd7e716fec15de368921dd0cd05b03
Xiongziwen <491032102@qq.com>
2017-06-17 20:43:36 +0800

    xzw011 HomeFragment constans

--- app/src/main/java/buy/win/com/winbuy/utils/Constans.java
+++ app/src/main/java/buy/win/com/winbuy/utils/Constans.java
@@ -7,6 +7,7 @@ public class Constans {
 
     public static final String URL_HOST = "http://192.168.21.100:8080/market/";
 
+
 //    public static final String URL_HOST = "http://www.wukimda.win:8080/market/";
 
     public static final int ERROR01= 1530;// 用户名不存在或密码错误
